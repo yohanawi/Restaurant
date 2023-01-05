e01797021b463be3619c8ba333b64a4ffbb9cb67
yohanawi <yohanawishka2000326@gmail.com>
2023-01-05 19:54:12 +0530

    commit
    navigation bar

--- Admin/components/navbar.php
+++ Admin/components/navbar.php
@@ -6,7 +6,19 @@
         <ul class="nav" id="side-menu">
             <li class="nav-small-cap m-t-10">--- Main Menu</li>
             <li> <a href="index.php" class="waves-effect active"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Dashboard </a></li>
-            <li><a href="#" class="waves-effect"><i data-icon="p" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Manage-Ticket</a></li>
+            <li><a href="#" class="waves-effect"><i data-icon="p" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Products</a></li>
+            <li><a href="#" class="waves-effect"><i data-icon="p" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Manage User</a></li>
+            <li><a href="#" class="waves-effect"><i data-icon="p" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Orders</a></li>
+            <li><a href="#" class="waves-effect"><i data-icon="p" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Messages</a></li>
+            <li><a href="#" class="waves-effect"><i data-icon="p" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Category</a></li>
+            <li class="nav-small-cap m-t-10">--- Reservation</li>
+            <li> <a href="#" class="waves-effect"><i data-icon="p" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Reservations<span class="fa arrow"></span></span></a>
+                <ul class="nav nav-second-level">
+                    <li><a href="#">Tables</a></li>
+                    <li><a href="#">Bookings</a></li>
+                </ul>
+            </li>
+            
             <li><a href="#" class="waves-effect"><i class="icon-logout fa-fw"></i> <span class="hide-menu">Log out</span></a></li>
         </ul>
     </div>
