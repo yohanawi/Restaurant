5c8633f0131c95cc586579e64b9dca2cb889f907
yohanawi <yohanawishka2000326@gmail.com>
2023-01-19 00:11:55 +0530

    commit

--- Order.php
+++ Order.php
@@ -57,7 +57,7 @@ if (isset($_SESSION['user_id'])) {
                                 <p>address : <span><?= $fetch_orders['address']; ?></span></p>
                                 <p>payment method : <span><?= $fetch_orders['method']; ?></span></p>
                                 <p>your orders : <span><?= $fetch_orders['total_products']; ?></span></p>
-                                <p>total price : <span>$<?= $fetch_orders['total_price']; ?>/-</span></p>
+                                <p>total price : <span>Rs. <?= $fetch_orders['total_price']; ?>/-</span></p>
                                 <p> payment status : <span style="color:<?php if ($fetch_orders['payment_status'] == 'pending') {
                                                                             echo 'red';
                                                                         } else {
